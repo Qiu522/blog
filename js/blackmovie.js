e3546702971ac10404c490aadb9cf23642b13e26
Qiu522 <1813778458@qq.com>
2021-06-15 01:27:00 +0800

    Update blackmovie.js

--- js/blackmovie.js
+++ js/blackmovie.js
@@ -2260,14 +2260,14 @@ var searchmovie = (lazyData, keydata)=>{
                 var{jpys_lazy, taotao_lazy, five_lazy, mjc_lazy, lengyue_lazy} = lazyData;
                 if(/meijuchong/.test(MY_URL)){
                     try{
-                        var list = parseDom(html, 'body&&.search_box&&.vodlist&&Html').match(/<li[\s\S]*?<\/li/g);
+                        var list = parseDomForArray(html, 'body&&.myui-vodlist__media&&li');
                         for (var j = 0; j < list.length; j++) {
                             d.push({
-                                title: parseDomForHtml(list[j], '.vodlist_thumb&&title'),
-                                desc: parseDomForHtml(list[j], '.pic_text&&Text'),
-                                pic_url: parseDom(list[j], '.vodlist_thumb&&data-original'),
-                                content:parseDomForHtml(list[j], '.searchlist_titbox&&Text'),
-                                url:$(parseDom(list[j], '.vodlist_thumb&&href')).rule((mjc_lazy) => { eval(fetch('hiker://files/rules/zyf/B_play.js')); mjc_lazy!=undefined? jx_mjc(mjc_lazy) : jx_mjc() }, mjc_lazy),
+                                title: parseDomForHtml(list[j], '.myui-vodlist__thumb&&title'),
+                                desc: parseDomForHtml(list[j], '.pic-text&&Text'),
+                                pic_url: parseDom(list[j], '.myui-vodlist__thumb&&data-original'),
+                                content:parseDomForHtml(list[j], '.hidden-xs&&Text'),
+                                url:$(parseDom(list[j], '.btn&&href')).rule((mjc_lazy) => { eval(fetch('hiker://files/rules/zyf/B_play.js')); mjc_lazy!=undefined? jx_mjc(mjc_lazy) : jx_mjc() }, mjc_lazy),
                             });
                         }
                     }catch(e){}
@@ -2673,16 +2673,16 @@ var searchmovie = (lazyData, keydata)=>{
                     if(searchType=='全部' || searchType=='美剧' || searchType=='美剧虫') {
                         var html = request(movielists[i].search.replace('关键词', key).replace('fypage','1'));
                         try{
-                            var list = parseDom(html, 'body&&.search_box&&.vodlist&&Html').match(/<li[\s\S]*?<\/li/g);
+                            var list = parseDomForArray(html, 'body&&.myui-vodlist__media&&li');
                             if(list == null) continue;
                             var len = list.length>6 ? 6 : list.length;
                             for (var j = 0; j < len; j++) {
                                 d.push({
-                                    title: parseDomForHtml(list[j], '.vodlist_thumb&&title'),
-                                    desc: parseDomForHtml(list[j], '.pic_text&&Text'),
-                                    pic_url: parseDom(list[j], '.vodlist_thumb&&data-original'),
-                                    content:parseDomForHtml(list[j], '.searchlist_titbox&&Text'),
-                                    url:$(parseDom(list[j], '.vodlist_thumb&&href')).rule((mjc_lazy) => { eval(fetch('hiker://files/rules/zyf/B_play.js')); mjc_lazy!=undefined? jx_mjc(mjc_lazy) : jx_mjc() }, mjc_lazy),
+                                    title: parseDomForHtml(list[j], '.myui-vodlist__thumb&&title'),
+                                    desc: parseDomForHtml(list[j], '.pic-text&&Text'),
+                                    pic_url: parseDom(list[j], '.myui-vodlist__thumb&&data-original'),
+                                    content:parseDomForHtml(list[j], '.hidden-xs&&Text'),
+                                    url:$(parseDom(list[j], '.btn&&href')).rule((mjc_lazy) => { eval(fetch('hiker://files/rules/zyf/B_play.js')); mjc_lazy!=undefined? jx_mjc(mjc_lazy) : jx_mjc() }, mjc_lazy),
                                 });
                             }
                         }catch(e){}
