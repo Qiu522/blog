a0474ac941a9fec1834406f0874ed4fb2472f603
Qiu522 <1813778458@qq.com>
2021-06-25 16:25:00 +0800

    Update blackmovie.js

--- js/blackmovie.js
+++ js/blackmovie.js
@@ -4653,6 +4653,24 @@ var getUpdateInfo = ()=>{
     }else if(/nfxhd/.test(MY_URL)){
         var time = parseDomForHtml(html, '.myui-content__detail&&p,1&&Text');
         setResult(time);
+    }else if(/mjhd/.test(MY_URL)){
+        var time = parseDomForHtml(html, '.myui-content__detail&&p,2&&Text');
+        setResult(time);
+    }else if(/dm45/.test(MY_URL)){
+        // 播放列表的列表的定位
+        var conts = parseDomForArray(html,'body&&.leo-play-num')[0];
+        // 选集列表的定位
+        var list=parseDomForArray(conts, 'ul&&li');
+        var title="";
+        // 过滤掉含番外和特别等字眼为最后一集的选集，避免有更新的选集无法被感知
+        for(let i = 1; i < list.length; i++) {
+            let index = list.length-i;
+            title = parseDomForHtml(list[index],'a&&Text');
+            if(title.search(/番外|特别/) == -1) break;
+        }
+        // 获取更新时间，确保有更新时能正常提示
+        //var time = parseDomForHtml(html, ".myui-content__detail&&p,4&&Text").replace("更新：", "");
+        setResult("更新至: " + title );
     }
 
 }
